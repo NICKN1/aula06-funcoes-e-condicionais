ba9c8d56a9bf03ba4540a326009d0c3f4115c9d6
NICKN1 <iranildo.junior@outlook.com>
2022-06-09 16:51:41 -0300

    Correção da Mesa de Trabalho

--- Mesa-De-Traba/Program.cs
+++ Mesa-De-Traba/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Variáveis
+double cotacaoDoDolarHoje = 4.97;
+// Constantes
+const double VALOR_MINIMO_DE_COMPRA_EM_DOLAR = 100;
+const double VALOR_MAXIMO_DE_COMPRA_EM_DOLAR = 2500;
+
+// Função
+double converterRealParaDolar(double valorDaCompraEmReais, double cotacaoDoDolarHoje)
+{
+    valorDaCompraEmReais /= cotacaoDoDolarHoje;
+
+    return valorDaCompraEmReais;
+}
+
+Console.WriteLine("Digite o valor da compra: ");
+
+//Chamar o método da função  (Os dois parametros)
+double valor = converterRealParaDolar(int.Parse(Console.ReadLine()), cotacaoDoDolarHoje);
+
+
+if (valor < VALOR_MINIMO_DE_COMPRA_EM_DOLAR)
+{
+    Console.WriteLine($"A compra mínima permitida é de $ {VALOR_MINIMO_DE_COMPRA_EM_DOLAR}");
+}
+else if (valor > VALOR_MAXIMO_DE_COMPRA_EM_DOLAR)
+{
+    Console.WriteLine($"A compra máxima permitida é de $ {VALOR_MAXIMO_DE_COMPRA_EM_DOLAR}");
+}
+else
+{
+    Console.WriteLine($"Você pode comprar $ {valor.ToString("N2")}");
+}
