da1f342e5bc2dfbd267468f227aed6bbc4b6fbfd
Danilo Hiroyuki Sanada <danilohsanada@hotmail.com>
2022-06-09 17:26:30 -0300

    Adicionado o programa do desafio do professor

--- Desafio-do-Professor/Program.cs
+++ Desafio-do-Professor/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string retornarDiagnosticoDoUsuario(double alturaDoUsuario, double pesoDoUsuario)
+{
+    double imc = pesoDoUsuario / (alturaDoUsuario * alturaDoUsuario);
+    if(imc < 18.5){
+        return "Anêmico";
+    }
+    else if(imc < 24.9) {
+        return "Normal";
+    }
+    else if(imc < 29.9) {
+        return "Sobreso";
+    }
+    else if(imc > 30 || imc < 40) {
+        return "Obesidade";
+    }
+    else{
+        return "Obesidade grave";
+    }
+}
+
+// Utilize a mensagem a seguir como base do retorno esperado:
+// Seu diagnóstico é XXXXXX.
+Console.WriteLine("Digite sua altura em metros e seu peso respectivamente:");
+Console.WriteLine($"Seu diagnóstico é {retornarDiagnosticoDoUsuario(double.Parse(Console.ReadLine()),double.Parse(Console.ReadLine()))} ");
